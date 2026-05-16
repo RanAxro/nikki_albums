2e881f39b857bb89dfbf3468c0e1fb80be4ad90d
RanAxro <2746984309@qq.com>
2026-05-16 21:34:25 +0800

    [refactor(ext_type)]

--- rust/src/nuan5_media_param/serde_nuan5_json/ext_type.rs
+++ rust/src/nuan5_media_param/serde_nuan5_json/ext_type.rs
@@ -7,29 +7,16 @@ pub const ID_MAP_TOKEN: &str = "IdMap";
 
 #[derive(Clone)]
 #[derive(Serialize, Deserialize)]
-pub struct IdMap<T>(HashMap<i64, T>);
+pub(crate) struct IdMap<T>(HashMap<i64, T>);
 
 impl<T> IdMap<T>{
-  pub fn new() -> Self{
+  pub(crate) fn new() -> Self{
     IdMap(HashMap::new())
   }
 }
 
-// impl IdMap<bool>{
-//   pub fn as_bool(&self) -> &HashMap<i64, bool>{
-//     &self.0
-//   }
-// }
-//
-// impl IdMap<i64>{
-//   pub fn as_i64(&self) -> &HashMap<i64, i64>{
-//     &self.0
-//   }
-// }
-
-
-impl<T: Copy> IdMap<T>{
-  pub fn as_map(&self) -> HashMap<i64, T>{
+impl<T: Clone> IdMap<T>{
+  pub(crate) fn as_map(&self) -> HashMap<i64, T>{
     self.0.clone()
   }
 }
@@ -47,28 +34,18 @@ impl<T> DerefMut for IdMap<T>{
   }
 }
 
-// #[frb(sync, positional)]
-// pub fn convert_to_bool_id_map(map: IdMap<bool>) -> HashMap<i64, bool>{
-//   map.as_bool().clone()
-// }
-// #[frb(sync, positional)]
-// pub fn convert_to_int_id_map(map: IdMap<i64>) -> HashMap<i64, i64>{
-//   map.as_i64().clone()
-// }
 
-
-// #[frb(non_opaque)]
 #[derive(Clone)]
 #[derive(Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum AdaptiveArray<T>{
+pub(crate) enum AdaptiveArray<T>{
   Array(Vec<T>),
   Item(T),
   Empty{},
 }
 
-impl<T: Copy> AdaptiveArray<T>{
-  pub fn to_vec(&self) -> Vec<T>{
+impl<T: Clone> AdaptiveArray<T>{
+  pub(crate) fn to_vec(&self) -> Vec<T>{
     match self{
       AdaptiveArray::Array(v) => {
         v.clone()
@@ -81,37 +58,10 @@ impl<T: Copy> AdaptiveArray<T>{
   }
 }
 
-
-
-// #[derive(Clone)]
-// #[derive(Serialize, Deserialize)]
-// pub struct EmptyMap{
-//
-// }
-
 #[derive(Clone)]
 #[derive(Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum OptionMap<T>{
+pub(crate) enum OptionMap<T>{
   None{},
   Some(T),
 }
-
-// #[macro_export]
-// macro_rules! option_map{
-//   ($($type:ty => $name:ident),+ $(,)?) => {
-//     $(
-//       #[derive(Clone)]
-//       #[derive(Serialize, Deserialize)]
-//       #[serde(untagged)]
-//       pub enum $name {
-//         None(EmptyMap),
-//         Some($type),
-//       }
-//     )+
-//   };
-// }
-//
-// option_map!{String => StringOptionMap, i64 => Int}
-
-
