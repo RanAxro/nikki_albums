aa7f9898bb1b37784ff8a6bbe8c04a0816b92a8a
RanAxro <2746984309@qq.com>
2026-05-16 21:12:00 +0800

    [feat(coverter)] convert_camera_params

--- rust/src/nuan5_media_param/converter.rs
+++ rust/src/nuan5_media_param/converter.rs
@@ -1,5 +1,6 @@
 use crate::nuan5_media_param::serde_nuan5_json::ext_type::{OptionMap};
 use crate::nuan5_media_param::serde_nuan5_json::structs::image_custom_data;
+use crate::nuan5_media_param::parser::camera_params_parser::*;
 use super::structs::nikki_photo_params::*;
 
 pub fn convert_nikki_photo_params(data: &image_custom_data::NikkiPhotoCustomData) -> NikkiPhotoParams{
@@ -43,6 +44,58 @@ fn convert_photography_params(data: &image_custom_data::NikkiPhotoCustomData) ->
       None => vec![],
     },
     task: convert_task_params(data),
+  }
+}
+
+fn convert_camera_params(data: &image_custom_data::SocialPhoto, portrait_data: &Option<image_custom_data::PortraitModeHandler>) -> CameraParams{
+  let params = parse_camera_params(data.camera_params.as_ref());
+
+  CameraParams{
+    params: data.camera_params.clone(),
+    portrait_mode: match portrait_data{
+      Some(portrait_mode_handler) => {
+        if portrait_mode_handler.portrait_mode == 1 { true } else { false }
+      },
+      None => false,
+    },
+    zoom: {
+      let i = &data.photo_info;
+      let dx = i.nikki_loc_x - i.camera_actor_loc_x;
+      let dy = i.nikki_loc_y - i.camera_actor_loc_y;
+      let dz = i.nikki_loc_z - i.camera_actor_loc_z;
+      let distance = (dx * dx + dy * dy + dz * dz).sqrt();
+      -0.0035 * distance + 6.45
+    },
+    focal_length: data.photo_info.camera_focal_length,
+    rotation: data.photo_info.camera_actor_rot_roll,
+    aperture_section: data.photo_info.aperture_section,
+    vignette_intensity: data.photo_info.vignette_intensity,
+    bloom_intensity: parse_bloom_intensity(&params),
+    bloom_threshold: parse_bloom_intensity(&params),
+    brightness: parse_brightness(&params),
+    exposure: parse_exposure(&params),
+    contrast: parse_contrast(&params),
+    saturation: parse_saturation(&params),
+    vibrance: parse_vibrance(&params),
+    highlights: parse_highlights(&params),
+    shadows: parse_shadows(&params),
+    light: if data.photo_info.light_id == "None" {
+      LightParams::None
+    }else{
+      LightParams::Some{
+        id: data.photo_info.light_id.clone(),
+        strength: data.photo_info.light_strength,
+      }
+    },
+    filter: if data.photo_info.filter_id == "None" {
+      FilterParams::None
+    }else{
+      FilterParams::Some{
+        id: data.photo_info.filter_id.clone(),
+        strength: data.photo_info.filter_strength,
+      }
+    },
+    pose: data.photo_info.pose_id,
   }
 }
 
