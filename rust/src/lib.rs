4c8fb151f83953983ed3e9120dc00e228c96db5e
RanAxro <2746984309@qq.com>
2026-05-20 00:15:46 +0800

    [feat(ue5_transform)] UE5 FTransform

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,4 @@ mod frb_generated;
 pub mod api;
 pub mod nuan5_media_param;
 pub mod thumbnail;
+pub mod ue5_transform;
---
+++ rust/src/ue5_transform/transform.rs
@@ -0,0 +1,5 @@
+pub struct FTransform{
+  rotation: (f64, f64, f64, f64),
+  translation: (f64, f64, f64),
+  scale: (f64, f64, f64),
+}
