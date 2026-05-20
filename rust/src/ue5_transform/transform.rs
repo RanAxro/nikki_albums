e3bd6e6e77de49dea8de7b8c3ae1028563dbfb67
RanAxro <2746984309@qq.com>
2026-05-20 14:20:27 +0800

    [feat(ue5_transform)] FTransform结构体

--- rust/src/ue5_transform/transform.rs
+++ rust/src/ue5_transform/transform.rs
@@ -1,5 +1,28 @@
-pub struct FTransform{
-  rotation: (f64, f64, f64, f64),
-  translation: (f64, f64, f64),
-  scale: (f64, f64, f64),
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone)]
+#[derive(Serialize, Deserialize)]
+pub struct FQuat{
+  pub x: f64,
+  pub y: f64,
+  pub z: f64,
+  pub w: f64,
 }
+
+#[derive(Clone)]
+#[derive(Serialize, Deserialize)]
+pub struct FVector{
+  pub x: f64,
+  pub y: f64,
+  pub z: f64,
+}
+
+#[derive(Clone)]
+#[derive(Serialize, Deserialize)]
+pub struct FTransform{
+  pub rotation: FQuat,
+  pub translation: FVector,
+  pub scale: FVector,
+}
+
+pub type FTransforms = Vec<FTransform>;
