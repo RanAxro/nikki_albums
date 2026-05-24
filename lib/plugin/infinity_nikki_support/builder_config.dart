87571dd262a57a1ec3066acc00946cb4bcb8f49c
RanAxro <2746984309@qq.com>
2026-05-25 00:52:24 +0800

    [feat(infinity_nikki_config)] 完成语言翻译

--- lib/plugin/infinity_nikki_support/builder_config.dart
+++ lib/plugin/infinity_nikki_support/builder_config.dart
@@ -1,12 +1,16 @@
 import "plugin_info.dart";
-import "language.dart";
+import "lang/zh-CN.dart";
+import "lang/en-US.dart";
 import "icon.dart";
 import "game_config.dart";
 import "package:nikki_albums/modules/plugin/model/plugin_builder_config.dart";
 
 const PluginBuilderConfig builderConfig = PluginBuilderConfig(
   info: pluginConfig,
-  languageList: languageList,
+  languageList: {
+    "zh-CN": zhCN,
+    "en-US": enUS,
+  },
   iconList: iconList,
   gameConfigList: [infinityNikkiConfig],
 );
