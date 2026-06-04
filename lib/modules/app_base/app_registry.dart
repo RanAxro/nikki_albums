527de1d90243e2b2714a75e8278099333f05fa94
Ran <2746984309@qq.com>
2026-06-05 02:23:09 +0800

    [refactor] 拆分语言文件

--- lib/modules/app_base/app_registry.dart
+++ lib/modules/app_base/app_registry.dart
@@ -1,6 +1,9 @@
 
 abstract class AppRegistry{
   static List<String> langFile = [
+    "infinity_nikki",
     "infinity_nikki/cloth",
+    "infinity_nikki/cloth_outfit",
+    "infinity_nikki/other",
   ];
 }
