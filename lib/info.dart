e3632447f042b1eb10a6bf22df5989785157fab3
Ran <2746984309@qq.com>
2026-06-28 20:22:22 +0800

    [update] 更新version info至v3.08.03

--- lib/info.dart
+++ lib/info.dart
@@ -3,8 +3,8 @@ import "dart:io";
 import "package:flutter/foundation.dart";
 import "package:win32_registry/win32_registry.dart";
 
-const int version = 15;
-const String versionString = "3.08.02";
+const int version = 16;
+const String versionString = "3.08.03";
 const String appName = "nikkialbums";
 const String appDefaultClassName = "FLUTTER_RUNNER_WIN32_WINDOW";
 const String appClassName = "RANAXRO_NIKKI_ALBUMS_WIN32_WINDOW";
