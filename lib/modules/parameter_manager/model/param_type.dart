51bb62fbddb4250b559b350186b6c80148e40c42
Ran <2746984309@qq.com>
2026-07-03 20:08:41 +0800

    [feat(parameter_manager)] 拓展param_type的方法

--- lib/modules/parameter_manager/model/param_type.dart
+++ lib/modules/parameter_manager/model/param_type.dart
@@ -1,13 +1,41 @@
 
 enum ParamType{
-  camera,
-  cloth,
-  home,
+  camera(0),
+  cloth(1),
+  home(2);
+
+  final int value;
+
+  const ParamType(this.value);
+
+  List<ParamSubType> get subType => paramTypeConfig[this]!;
+
+  static ParamType? fromValue(dynamic v){
+    return ParamType.values.cast<ParamType?>().firstWhere(
+      (ParamType? type) => type?.value == v,
+      orElse: () => null,
+    );
+  }
+
+  int toValue() => value;
 }
 
 enum ParamSubType{
-  island,
-  group,
+  island(0),
+  group(1);
+
+  final int value;
+
+  const ParamSubType(this.value);
+
+  static ParamSubType? fromValue(dynamic v){
+    return ParamSubType.values.cast<ParamSubType?>().firstWhere(
+      (ParamSubType? type) => type?.value == v,
+      orElse: () => null,
+    );
+  }
+
+  int toValue() => value;
 }
 
 const Map<ParamType, List<ParamSubType>> paramTypeConfig = {
