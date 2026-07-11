5f844f8102e798e43b6ec35507b196152573d344
Ran <2746984309@qq.com>
2026-07-11 17:54:33 +0800

    [feat(parameter_manager)] 家园参数面板增加前往搬砖吧大喵可视化按钮

--- lib/modules/parameter_manager/presentation/rich_building_params_panel.dart
+++ lib/modules/parameter_manager/presentation/rich_building_params_panel.dart
@@ -7,6 +7,7 @@ import "package:nikki_albums/widgets/app/component.dart";
 import "package:nikki_albums/widgets/common/component.dart";
 
 import "package:flutter/material.dart";
+import "package:url_launcher/url_launcher_string.dart";
 
 
 class RichBuildingParamsPanel extends StatelessWidget{
@@ -26,60 +27,98 @@ class RichBuildingParamsPanel extends StatelessWidget{
     return SmoothPointerScroll(
       builder: (BuildContext context, ScrollController scrollController, ScrollPhysics physics, IndependentScrollbarController scrollbarController){
         return SingleChildScrollView(
-          child: Table(
-            columnWidths: const {
-              0: IntrinsicColumnWidth(),
-              1: FlexColumnWidth(),
-            },
-            border: TableBorder.all(
-              color: AppColorScheme.of(context).byRole(ColorRole.of(context)).hoveredColor,
-            ),
+          child: Column(
+            spacing: listSpacing,
             children: [
-              TableRow(
+              Table(
+                columnWidths: const {
+                  0: IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                border: TableBorder.all(
+                  color: AppColorScheme.of(context).byRole(ColorRole.of(context)).hoveredColor,
+                ),
                 children: [
-                  AppText(trText("home_build_data.cover_image")),
+                  TableRow(
+                    children: [
+                      AppText(trText("home_build_data.cover_image")),
 
-                  if(richBuildingParams.coverImage == null)
-                    AppText(trBool(false, index: 2)),
+                      if(richBuildingParams.coverImage == null)
+                        AppText(trBool(false, index: 2)),
 
-                  if(richBuildingParams.coverImage != null)
-                    Image.network(richBuildingParams.coverImage!, width: 200),
-                ],
+                      if(richBuildingParams.coverImage != null)
+                        Image.network(richBuildingParams.coverImage!, width: 200),
+                    ],
+                  ),
+
+                  TableRow(
+                    children: [
+                      AppText(trText("home_build_data.name")),
+                      AppText(richBuildingParams.name),
+                    ],
+                  ),
+
+                  TableRow(
+                    children: [
+                      AppText(trText("home_build_data.version")),
+                      AppText(richBuildingParams.version),
+                    ],
+                  ),
+
+                  TableRow(
+                    children: [
+                      AppText(trText("home_build_data.furniture_count")),
+                      AppText(richBuildingParams.furnitureCount.toString()),
+                    ],
+                  ),
+
+                  TableRow(
+                    children: [
+                      AppText(trText("home_build_data.server")),
+                      AppText(trText(HomeBuildShareCode.fromCodeStr(shareCode).server().toString(), category: "share_code_server")),
+                    ],
+                  ),
+                ].map((TableRow tableRow) => TableRow(
+                  children: tableRow.children.map((child) => Padding(
+                    padding: const EdgeInsets.all(smallPadding),
+                    child: child,
+                  )).toList(),
+                )).toList(),
               ),
 
-              TableRow(
-                children: [
-                  AppText(trText("home_build_data.name")),
-                  AppText(richBuildingParams.name),
-                ],
-              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IntrinsicWidth(
+                  child: AppButton(
+                    toolTip: trText("home_build_data.open_building_momo"),
+                    isTranslate: false,
+                    borderRadius: smallBorderRadius,
+                    padding: const EdgeInsets.symmetric(horizontal: smallPadding),
+                    height: mediumButtonSize,
+                    isTransparent: false,
+                    onClick: () async{
+                      final String url = "https://infinitymomo.com?schemeCode=$shareCode";
 
-              TableRow(
-                children: [
-                  AppText(trText("home_build_data.version")),
-                  AppText(richBuildingParams.version),
-                ],
+                      if(await canLaunchUrlString(url)){
+                        launchUrlString(url);
+                      }else{
+                        AppToast.showMessage(
+                          context: context,
+                          message: "",
+                        );
+                      }
+                    },
+                    child: Row(
+                      spacing: listSpacing,
+                      children: [
+                        AppIcon("building_momo", width: 30, height: 30, isDye: false),
+                        AppText(trText("home_build_data.visualization")),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-
-              TableRow(
-                children: [
-                  AppText(trText("home_build_data.furniture_count")),
-                  AppText(richBuildingParams.furnitureCount.toString()),
-                ],
-              ),
-
-              TableRow(
-                children: [
-                  AppText(trText("home_build_data.server")),
-                  AppText(trText(HomeBuildShareCode.fromCodeStr(shareCode).server().toString(), category: "share_code_server")),
-                ],
-              ),
-            ].map((TableRow tableRow) => TableRow(
-              children: tableRow.children.map((child) => Padding(
-                padding: const EdgeInsets.all(smallPadding),
-                child: child,
-              )).toList(),
-            )).toList(),
+            ],
           ),
         );
       },
