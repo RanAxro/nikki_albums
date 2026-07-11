ced8ea1d014f510f346cf2baaeec392e7691b2ee
Ran <2746984309@qq.com>
2026-07-11 14:51:32 +0800

    [feat(nuan5_params)] 定义DyeCondition枚举

---
+++ lib/modules/nuan5_params/model/cloth_diy.dart
@@ -0,0 +1,21 @@
+
+enum DyeCondition implements Comparable<DyeCondition>{
+  directly(0),
+  complete(1),
+  growUp(2),
+  evolution_1(3),
+  evolution_2(4),
+  evolution_3(5);
+
+  final int value;
+
+  const DyeCondition(this.value);
+
+  @override
+  int compareTo(DyeCondition other) => value.compareTo(other.value);
+
+  bool operator <(DyeCondition other) => value < other.value;
+  bool operator >(DyeCondition other) => value > other.value;
+  bool operator <=(DyeCondition other) => value <= other.value;
+  bool operator >=(DyeCondition other) => value >= other.value;
+}
