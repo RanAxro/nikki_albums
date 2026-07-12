a4e35f7125dc1977f4831ec6133b075f34e859f7
Ran <2746984309@qq.com>
2026-07-13 03:34:55 +0800

    [feat(parameter_manager)] ParamItemCreator

--- lib/modules/parameter_manager/domain/param_item_creator.dart
+++ lib/modules/parameter_manager/domain/param_item_creator.dart
@@ -9,17 +9,77 @@ import "package:nikki_albums/src/rust/nuan5_params/structs/building_params.dart"
 
 import "package:flutter/foundation.dart";
 import "dart:convert";
+import "dart:async";
 
 import "package:path/path.dart" as p;
 
 
-class ParamItemCreator{
+class ParamItemCreator extends ChangeNotifier{
   ParamItemCreator();
 
-  String? _param;
-  ParamType? _type;
+  String _paramString = "";
+  dynamic _param;
+  ParamType _type = ParamType.camera;
 
+  String get paramString => _paramString;
+  dynamic get param => _param;
+  ParamType get type => _type;
 
+  void setParamString(String newParamString){
+    _paramString = newParamString;
+  }
+
+  void changeParamString(String newParamString) async{
+    if(newParamString == _paramString){
+      return;
+    }
+
+    tryDeParamDebounce(newParamString, onFinished: (dynamic param){
+      _param = param;
+
+      if(_param is CameraParams){
+        _type = ParamType.camera;
+      }
+      if(_param is ClothDiyParams){
+        _type = ParamType.cloth;
+      }
+      if(_param is RichBuildingParams){
+        _type = ParamType.home;
+      }
+
+      notifyListeners();
+    });
+  }
+
+  void changeType(ParamType newType) async{
+    if(newType == _type){
+      return;
+    }
+
+    _type = newType;
+
+    switch(newType){
+      case ParamType.camera:
+        _param = await tryDeCameraParam(_paramString);
+        break;
+      case ParamType.cloth:
+        if(_paramString.length < 2 || _paramString.length >= 64){
+          _param = null;
+          break;
+        }
+        _param = await tryDeClothDiyShareCode(_paramString);
+        break;
+      case ParamType.home:
+        if(_paramString.length < 2 || _paramString.length >= 64){
+          _param = null;
+          break;
+        }
+        _param = await tryDeHomeBuildShareCode(_paramString);
+        break;
+    }
+
+    notifyListeners();
+  }
 }
 
 Future<dynamic> tryDeParam(String param) async{
@@ -41,6 +101,32 @@ Future<dynamic> tryDeParam(String param) async{
   }
 }
 
+
+Timer? _tryDeParamTimer;
+void tryDeParamDebounce(String param, {Duration duration = const Duration(milliseconds: 400), void Function(dynamic)? onFinished}){
+  _tryDeParamTimer?.cancel();
+
+  if(param.length < 2){
+    return onFinished?.call(null);
+  }
+
+  /// Camera Params
+  if(param.length >= 64){
+    tryDeCameraParam(param).then((CameraParams? value) => onFinished?.call(value));
+  }else{
+    _tryDeParamTimer = Timer(duration, () async{
+      /// Cloth Diy Share Code
+      if(param.endsWith("#")){
+        onFinished?.call(await tryDeClothDiyShareCode(param));
+      }
+      /// Home Build Share Code or Cloth Diy Share Code
+      else{
+        onFinished?.call(await tryDeHomeBuildShareCode(param) ?? await tryDeClothDiyShareCode(param));
+      }
+    });
+  }
+}
+
 Future<CameraParams?> tryDeCameraParam(String param) async{
   try{
     final MediaKey key = MediaKey.cameraParam();
@@ -58,10 +144,6 @@ Future<CameraParams?> tryDeCameraParam(String param) async{
       }
     );
   }catch(e){
-    if(kDebugMode){
-      rethrow;
-    }
-
     return null;
   }
 }
@@ -80,10 +162,6 @@ Future<ClothDiyParams?> tryDeClothDiyShareCode(String code) async{
       },
     );
   }catch(e){
-    if(kDebugMode){
-      rethrow;
-    }
-
     return null;
   }
 }
@@ -102,10 +180,6 @@ Future<RichBuildingParams?> tryDeHomeBuildShareCode(String code) async{
       },
     );
   }catch(e){
-    if(kDebugMode){
-      rethrow;
-    }
-
     return null;
   }
 }
