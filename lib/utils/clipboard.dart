ca8fd7c4e862a191d666705a09ddd4d616e70598
Ran <2746984309@qq.com>
2026-07-13 03:16:24 +0800

    [feat(utils)] 加入读取剪切板图片功能

--- lib/utils/clipboard.dart
+++ lib/utils/clipboard.dart
@@ -1,6 +1,10 @@
-import "package:flutter/services.dart";
 
 import "path.dart";
+
+import "package:flutter/cupertino.dart";
+import "package:flutter/services.dart";
+import "dart:async";
+import "dart:io";
 
 import "package:super_clipboard/super_clipboard.dart";
 
@@ -55,4 +59,55 @@ Future<List<Path>> readFilesFromClipboard() async {
     }
   }
   return res;
+}
+
+
+
+Future<Uint8List?> readImageFromClipboard({String? savePath}) async{
+  final SystemClipboard? clipboard = SystemClipboard.instance;
+  if(clipboard == null) return null;
+
+  final ClipboardReader reader = await clipboard.read();
+
+  final List<SimpleFileFormat> formats = [
+    Formats.png,
+    Formats.jpeg,
+    Formats.bmp,
+    Formats.tiff,
+    Formats.gif,
+  ];
+
+  for(final SimpleFileFormat format in formats){
+    if(!reader.canProvide(format)) continue;
+
+    final Completer completer = Completer<Uint8List?>();
+
+    reader.getFile(format, (file) async{
+      try{
+        final bytes = await file.readAll();
+        completer.complete(bytes);
+      }catch(e){
+        completer.complete(null);
+      }
+    });
+
+    // 超时保护，防止回调不触发导致永久挂起
+    Future.delayed(const Duration(seconds: 5), (){
+      if(!completer.isCompleted) completer.complete(null);
+    });
+
+    final Uint8List? imageBytes = await completer.future;
+
+    if(imageBytes != null && savePath != null){
+      try{
+        await File(savePath).writeAsBytes(imageBytes);
+      }catch(e){
+        debugPrint("保存图片失败: $e");
+      }
+    }
+
+    return imageBytes;
+  }
+
+  return null;
 }
