dd1a8f4b65baa142bd96963a2f82120841efa208
Ran <2746984309@qq.com>
2026-07-13 19:07:58 +0800

    [feat(parameter_manager)] 完善ParamBoxManager功能

--- lib/modules/parameter_manager/domain/param_box_manager.dart
+++ lib/modules/parameter_manager/domain/param_box_manager.dart
@@ -1,30 +1,56 @@
 
+import "package:nikki_albums/modules/album/album.dart";
 
+import "../model/param_item.dart";
 import "../model/param_box.dart";
+import "package:nikki_albums/utils/system/system.dart";
 
 import "dart:io";
-import "dart:typed_data";
+import "package:flutter/foundation.dart";
 
 import "package:path/path.dart" as p;
 import "package:uuid/v4.dart";
 import "package:msgpack_dart/msgpack_dart.dart" as msgpack;
 
 
-class ParamBoxManager{
-  final File file;
 
-  ParamBoxManager(this.file);
+class ParamBoxManager extends ChangeNotifier{
+  static ParamBoxManager? _defaultParamBox;
+
+  static Future<String> getDefaultParamBoxPath() async{
+    final String basePath = (await getAppDataDirectoryPath()).path;
+    return p.join(basePath, "ParamBox");
+  }
+
+  static Future<ParamBoxManager> getDefaultParamBox() async{
+    _defaultParamBox ??= ParamBoxManager(Directory(await getDefaultParamBoxPath()));
+
+    return _defaultParamBox!;
+  }
+
+
+  final Directory directory;
+  final File manifestFile;
+
+  ParamBoxManager(this.directory) : manifestFile = File(p.join(directory.path, "manifest"));
 
   late final ParamBox _box;
 
+  bool isInit = false;
+
   Future<void> init() async{
-    if(await file.exists()){
-      final Uint8List bytes = await file.readAsBytes();
+    if(await manifestFile.exists()){
+      final Uint8List bytes = await manifestFile.readAsBytes();
       final dynamic data = msgpack.deserialize(bytes);
-      _box = ParamBox.fromMsgpackMap(data) as ParamBox;
+      final ParamBox? boxOr = ParamBox.fromMsgpackMap(data);
+      if(boxOr == null){
+        return;
+      }
+      _box = boxOr;
     }else{
       _box = ParamBox(set: [], tag: [], item: []);
     }
+    isInit = true;
   }
 
   Future<void> save() async{
@@ -32,10 +58,92 @@ class ParamBoxManager{
     final Uint8List bytes = msgpack.serialize(data);
 
     final String filename = UuidV4().generate();
-    final String filePath = p.join(file.parent.path, filename);
+    final String filePath = p.join(directory.path, filename);
     final File saveFile = File(filePath);
+    await saveFile.create(recursive: true);
     await saveFile.writeAsBytes(bytes);
 
-    await saveFile.rename(file.path);
+    await saveFile.rename(manifestFile.path);
+  }
+
+  List<ParamItem> get items => _box.item.where((ParamItem item) => !item.delete).toList();
+
+  String _generateAvailableUuid(){
+    final Set<String> unusable = _box.item.map((ParamItem item) => item.uuid).toSet();
+    while(true){
+      final String uuid = UuidV4().generate();
+      if(!unusable.contains(uuid)){
+        return uuid;
+      }
+    }
+  }
+
+  /// 文件后缀名不影响图片查看器识别, 这里统一使用 .png 后缀
+  String getImagePath(String uuid) => p.join(directory.path, "image", "$uuid.png");
+
+  Future<bool> addImage(String uuid, String source) async{
+    final String targetPath = getImagePath(uuid);
+    final File targetFile = File(targetPath);
+
+    final File sourceFile = File(source);
+    try{
+      if(!await targetFile.exists()){
+        await targetFile.create(recursive: true);
+      }
+      if(await sourceFile.exists()){
+        await sourceFile.copy(targetPath);
+        return true;
+      }else{
+        return false;
+      }
+    }catch(e, s){
+      if(kDebugMode){
+        print(e);
+        debugPrintStack(stackTrace: s);
+      }
+      return false;
+    }
+  }
+
+  Future<void> createItem(ParamItemCreation creation) async{
+    final String uuid = _generateAvailableUuid();
+    final int time = DateTime.timestamp().millisecondsSinceEpoch;
+
+    bool hasImage = false;
+    if(creation.originImagePath != null){
+      hasImage = await addImage(uuid, creation.originImagePath!);
+    }
+
+    _box.item.add(ParamItem(
+      uuid: uuid,
+      delete: false,
+      type: creation.type,
+      value: creation.value,
+      createdTime: time,
+      modifiedTime: time,
+      top: creation.top,
+      title: creation.title,
+      description: creation.description,
+      tag: creation.tag,
+      set: creation.set,
+      originImagePath: creation.originImagePath,
+      image: hasImage ? uuid : null,
+    ));
+
+    notifyListeners();
+  }
+
+  bool deleteItem(String uuid){
+    for(final item in _box.item){
+      if(item.uuid == uuid){
+        item.delete = true;
+
+        notifyListeners();
+        return true;
+      }
+    }
+
+    notifyListeners();
+    return false;
   }
 }
