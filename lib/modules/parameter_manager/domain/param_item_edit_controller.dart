bc8cf514de4d5c79af342e3f5e9088752be4742a
Ran <2746984309@qq.com>
2026-07-15 22:14:07 +0800

    [feat(aprameter_manager)] 实现ParamItemEditController

---
+++ lib/modules/parameter_manager/domain/param_item_edit_controller.dart
@@ -0,0 +1,120 @@
+
+import "package:nikki_albums/src/rust/nuan5_params/structs/building_params.dart";
+
+import "../model/param_type.dart";
+import "code_parser.dart";
+
+import "package:flutter/material.dart";
+
+
+abstract class ParamItemCover{
+  const ParamItemCover();
+}
+
+class NativeParamItemCover extends ParamItemCover{
+  final String path;
+  final bool isCache;
+
+  const NativeParamItemCover({required this.path, required this.isCache});
+}
+
+class NetworkParamItemCover extends ParamItemCover{
+  final String url;
+
+  const NetworkParamItemCover({required this.url});
+}
+
+
+class ParamItemEditController extends ChangeNotifier{
+  final String? initName;
+  final String? initCode;
+  final ParamItemCover? initCover;
+  final ParamType? initParamType;
+  final bool createMode;
+
+  ParamItemEditController({
+    this.initName,
+    this.initCode,
+    this.initCover,
+    this.initParamType,
+    this.createMode = false,
+  }){
+    _init();
+  }
+
+
+  final TextEditingController nameTextController = TextEditingController();
+  final TextEditingController codeTextController = TextEditingController();
+  dynamic _param;
+  final ValueNotifier<ParamItemCover?> cover = ValueNotifier(null);
+  ParamType _paramType = ParamType.camera;
+
+  void onCodeTextChanged(){
+    tryDeCodeDebounce(codeTextController.text, onFinished: _setParam);
+  }
+
+  Future<void> _init() async{
+    if(initCode == null){
+      if(initParamType != null) _paramType = initParamType!;
+      if(initName != null) nameTextController.text = initName!;
+      if(initCover != null) cover.value = initCover!;
+    }else{
+      codeTextController.text = initCode!;
+      if(initParamType == null){
+        final dynamic param = await tryDeCode(initCode!);
+        _setParam(param);
+      }else{
+        await setParamType(initParamType!);
+      }
+
+      if(initName != null){
+        nameTextController.text = initName!;
+      }
+      if(initCover != null){
+        cover.value = initCover;
+      }
+    }
+
+    codeTextController.addListener(onCodeTextChanged);
+  }
+
+  dynamic get param => _param;
+  ParamType get paramType => _paramType;
+
+  Future<void> setParamType(ParamType newParamType) async{
+    if(newParamType == _paramType){
+      return;
+    }
+
+    final dynamic newParam = await tryDeByType(_paramType, codeTextController.text);
+    _setParam(newParam);
+  }
+
+  void _setParam(dynamic newParam){
+    _param = newParam;
+    final ParamType? newType = getTypeByParam(newParam);
+    if(newType != null){
+      _paramType = newType;
+    }
+
+    /// 自动填充家园建造参数的方案名与方案名陈
+    if(newType == ParamType.home){
+      final RichBuildingParams homeBuildingParams = newParam as RichBuildingParams;
+
+      nameTextController.text = homeBuildingParams.name;
+      if(homeBuildingParams.coverImage != null){
+        cover.value = NetworkParamItemCover(url: homeBuildingParams.coverImage!);
+      }
+    }
+
+    notifyListeners();
+  }
+
+  @override
+  void dispose(){
+    nameTextController.dispose();
+    codeTextController.dispose();
+    cover.dispose();
+    super.dispose();
+  }
+}
