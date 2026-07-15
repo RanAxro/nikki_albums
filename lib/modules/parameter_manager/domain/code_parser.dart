6e73e96977c74f74b0cff2f0bd131b6c7cf57848
Ran <2746984309@qq.com>
2026-07-15 21:09:09 +0800

    [refactor(parameter_manager)] 提取核心解析函数

---
+++ lib/modules/parameter_manager/domain/code_parser.dart
@@ -0,0 +1,124 @@
+
+import "package:nikki_albums/utils/system/system.dart";
+import "package:nikki_albums/src/rust/nuan5_params/decrypt.dart";
+import "package:nikki_albums/src/rust/nuan5_params/decode.dart";
+import "package:nikki_albums/src/rust/nuan5_params/structs/camera_params.dart";
+import "package:nikki_albums/src/rust/nuan5_params/structs/cloth_diy_params.dart";
+import "package:nikki_albums/src/rust/nuan5_params/structs/building_params.dart";
+
+import "dart:convert";
+import "dart:async";
+
+import "package:path/path.dart" as p;
+
+
+Future<dynamic> tryDeCode(String code) async{
+  if(code.length < 2){
+    return null;
+  }
+
+  /// Camera Parameter
+  if(code.length >= 64){
+    return await tryDeCameraParameter(code);
+  }
+  /// Cloth Diy Share Code
+  else if(code.endsWith("#")){
+    return await tryDeClothDiyShareCode(code);
+  }
+  /// Home Build Share Code or Cloth Diy Share Code
+  else{
+    return await tryDeHomeBuildShareCode(code) ?? await tryDeClothDiyShareCode(code);
+  }
+}
+
+
+Timer? _tryDeCodeTimer;
+void tryDeCodeDebounce(String code, {Duration duration = const Duration(milliseconds: 400), void Function(dynamic)? onFinished}){
+  _tryDeCodeTimer?.cancel();
+
+  if(code.length < 2){
+    return onFinished?.call(null);
+  }
+
+  /// Camera Parameter
+  if(code.length >= 64){
+    tryDeCameraParameter(code).then((CameraParams? value) => onFinished?.call(value));
+  }else{
+    _tryDeCodeTimer = Timer(duration, () async{
+      /// Cloth Diy Share Code
+      if(code.endsWith("#")){
+        onFinished?.call(await tryDeClothDiyShareCode(code));
+      }
+      /// Home Build Share Code or Cloth Diy Share Code
+      else{
+        onFinished?.call(await tryDeHomeBuildShareCode(code) ?? await tryDeClothDiyShareCode(code));
+      }
+    });
+  }
+}
+
+Future<CameraParams?> tryDeCameraParameter(String code) async{
+  try{
+    final MediaKey key = MediaKey.cameraParam();
+
+    final CustomData data = mediaDecrypt(utf8.encode(code), key);
+    final MediaCustomData mediaCustomData = await decodeMediaParam(paramType: MediaParamType.cameraParams, data: data);
+
+    return mediaCustomData.whenOrNull(
+      valid: (MediaParam mediaParam){
+        return mediaParam.whenOrNull(
+          cameraParams: (CameraParams cameraParams){
+            return cameraParams;
+          },
+        );
+      },
+    );
+  }catch(e){
+    return null;
+  }
+}
+
+Future<ClothDiyParams?> tryDeClothDiyShareCode(String code) async{
+  try{
+    final String cachePath = await getClothDiyShareCodeTemp(code);
+
+    final ClothDiyShareCode key = ClothDiyShareCode.fromCodeStr(code);
+
+    final ClothDiyParam? clothDiyParam = await clothDiyDeNetwork(key: key, cachePath: cachePath);
+
+    return clothDiyParam?.whenOrNull(
+      clothDiy: (ClothDiyParams clothDiyParams){
+        return clothDiyParams;
+      },
+    );
+  }catch(e){
+    return null;
+  }
+}
+
+Future<RichBuildingParams?> tryDeHomeBuildShareCode(String code) async{
+  try{
+    final String cachePath = await getHomeBuildShareCodeTemp(code);
+
+    final HomeBuildShareCode key = HomeBuildShareCode.fromCodeStr(code);
+
+    final HomeBuildParam? homeBuildParam = await homeBuildDeNetwork(key: key, cachePath: cachePath);
+
+    return homeBuildParam?.whenOrNull(
+      netHomeBuild: (RichBuildingParams richBuildingParams){
+        return richBuildingParams;
+      },
+    );
+  }catch(e){
+    return null;
+  }
+}
+
+
+Future<String> getClothDiyShareCodeTemp(String code) async{
+  return p.join((await getTempPath()).path, "ClothDiyShareCode", code);
+}
+
+Future<String> getHomeBuildShareCodeTemp(String code) async{
+  return p.join((await getTempPath()).path, "HomeBuildShareCode", code);
+}
