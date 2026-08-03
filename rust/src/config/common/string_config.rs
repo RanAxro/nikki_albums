0fe8d7142fa80984f067db90947e3971d39dc038
Ran <2746984309@qq.com>
2026-08-03 19:59:26 +0800

    [feat(config)] 实现string_config

---
+++ rust/src/config/common/string_config.rs
@@ -0,0 +1,49 @@
+use flutter_rust_bridge::frb;
+use serde::{Deserialize, Serialize};
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+pub struct StringConfig{
+  pub process: Vec<StringProcessConfig>,
+}
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum StringProcessConfig{
+  Join(StringJoinProcessConfig),
+  Match(StringMatchProcessConfig),
+  Replace(StringReplaceProcessConfig),
+  ReplaceAll(StringReplaceAllProcessConfig),
+}
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+pub struct StringJoinProcessConfig{
+  pub symbol: String,
+  pub target: Vec<Vec<StringProcessConfig>>,
+}
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+pub struct StringMatchProcessConfig{
+  pub regex: String,
+  pub which: i32,
+  pub successful: String,
+  pub failed: Vec<StringProcessConfig>,
+}
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+pub struct StringReplaceProcessConfig{
+  pub regex: String,
+  pub which: i32,
+  pub to: String,
+}
+
+#[frb]
+#[derive(Serialize, Deserialize)]
+pub struct StringReplaceAllProcessConfig{
+  pub regex: String,
+  pub to: String,
+}
